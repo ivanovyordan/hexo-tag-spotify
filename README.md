d6477b92bb01d3914a0078db83397be09b21a30f
Yordan Ivanov <mail@ivanovyordan.com>
2015-04-06 09:08:49 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# About #
+
+A [Hexo](http://hexo.io/) tag plugin which makes it easy to embed any type of [Spotify](https://www.spotify.com) content.
+
+
+# Installation #
+
+```sh
+npm install --save hexo-tag-spotify
+```
+
+
+# Usage #
+
+Add the tag markup to a post with the url of the content you want to embed.
+
+1. Example with Spotify URI:
+```
+{% spotify:track:7kkRhGDCAVd5YVoieTChxG %}
+```
+
+2. Example with HTTP link:
+```
+{% http://open.spotify.com/artist/5p9CTsn5ueGU4oScNX1axu %}
+```
+
+# Configuration #
+
+You can configure the size, theme and view of the widgets.
+
+Allowed values are:
+1. size
+	* large (default)
+	* small
+2. theme
+	* dark (default)
+	* light
+3. view
+	* list (default)
+	* cover
+
+Example configuration:
+```yml
+spotify:
+  size: 'large'
+  theme: 'light'
+  view: 'list'
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+hexo.extend.tag.register('spotify', function(args) {
+	var url = args[0];
+	var src = 'https://embed.spotify.com/?uri=';
+	var isTrack = true;
+	var size = null;
+
+	var config = hexo.config.spotify || {};
+	config.size = config.size || 'large';
+	config.theme = config.theme || 'dark';
+	config.view = config.view || 'list';
+
+	if(url.indexOf('http://open.spotify.com/') === 0) {
+		url = url.replace('http://open.spotify.com/', '');
+		url = url.replace('/', ':');
+		url = 'spotify:' + url;
+	}
+
+	if(config.size === 'large') {
+		size = '300x380';
+	} else if(size === 'compact') {
+		size = '300x80';
+	}
+	size = size.split('x');
+
+	isTrack = url.split(':')[1] === 'track';
+	src += encodeURIComponent(url);
+
+	if(!isTrack && config.theme === 'light') {
+		src += '&amp;theme=white';
+	}
+
+	if(!isTrack && config.view === 'cover') {
+		src += '&amp;view=coverart';
+	}
+
+	var tag = '<iframe src="' + src + '"';
+	tag += ' width="' + size[0] + '" height="' + size[1] + '"';
+	tag += ' frameborder="0" allowtransparency="true"';
+	tag += ' class="spotify">';
+	tag += '</iframe>';
+
+	return tag;
+});
